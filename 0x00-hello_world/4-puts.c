aece7e34f1d389fa35b77facd1e62debc0ef840d
raoudha123456789 <romdhani.raoudha6@gmail.com>
2023-08-10 14:13:08 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
  * main - Entry point
  */
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+{ 
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
