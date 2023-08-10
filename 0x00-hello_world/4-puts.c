e711867639797e79772daf0ff839f8328293b88c
raoudha123456789 <romdhani.raoudha6@gmail.com>
2023-08-10 14:42:27 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+*main - prints a text using printf function
+*Return: always 0
+*/
 int main(void)
 { 
+
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
+
 }
