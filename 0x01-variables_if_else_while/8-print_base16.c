17c061c1a977df17e4115011e7fdd23cb5795862
raoudha123456789 <romdhani.raoudha6@gmail.com>
2023-08-11 13:26:59 -0700

    putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints to string
+ * Description: Prints all the numbers of base 16 in lowercase
+ * Return: 1
+ */
+int main(void)
+{
+char digit;
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+}
+for (digit = 'a'; digit <= 'f'; digit++)
+{
+putchar(digit);
+}
+putchar('\n');
+return (0);
+}
