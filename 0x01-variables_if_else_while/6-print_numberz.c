7ee965758b79cde9b358575a16e090c11e4471f7
raoudha123456789 <romdhani.raoudha6@gmail.com>
2023-08-11 14:58:07 -0700

    if

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 /**
- * main - prints to string
- * Description: Prints ll single digit numbers of base 10
- * Return: 0
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-putchar('0' + 0);
-putchar('0' + 1);
-putchar('0' + 2);
-putchar('0' + 3);
-putchar('0' + 4);
-putchar('0' + 5);
-putchar('0' + 6);
-putchar('0' + 7);
-putchar('0' + 8);
-putchar('0' + 9);
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
 putchar('\n');
 return (0);
 }
