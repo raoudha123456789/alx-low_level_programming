eb7719420bb41a5dee096649d723632b1317dd0a
raoudha123456789 <romdhani.raoudha6@gmail.com>
2023-08-11 09:45:21 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints to string
+ * Description: Prints ll single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+putchar('0');
+putchar('1');
+putchar('2');
+putchar('3');
+putchar('4');
+putchar('5');
+putchar('6');
+putchar('7');
+putchar('8');
+putchar('9');
+putchar('\n');
+return (0);
+}
