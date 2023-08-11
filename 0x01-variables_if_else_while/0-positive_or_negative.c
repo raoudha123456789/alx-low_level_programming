9fd2f7a46bf2014b039bb9ae3dfd04b2e6cae648
raoudha123456789 <romdhani.raoudha6@gmail.com>
2023-08-11 04:26:51 -0700

    if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,17 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("The number %d is ", n);
 if (n > 0)
+{
 printf("positive");
+}
 else if (n == 0)
+{
 printf("zero");
+}
 else
+{	
 printf("negative");
+}
 printf("\n");
 return (0);
 }
