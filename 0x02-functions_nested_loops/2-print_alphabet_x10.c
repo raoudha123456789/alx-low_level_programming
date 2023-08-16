1a4fc39b04592eefcd7cbbc44949b8bfa98ad0a3
raoudha123456789 <romdhani.raoudha6@gmail.com>
2023-08-16 13:40:36 -0700

    pf

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints the lowercase alphabet ten times followed by new lines
+ * print_alphabet_x10 - Prints the lowercase alphabets
  **/
 void print_alphabet_x10(void)
 {
