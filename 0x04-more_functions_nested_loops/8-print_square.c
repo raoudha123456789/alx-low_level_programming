4bcb0191e0f31961dce1ed1c66941d29817b6793
raoudha123456789 <romdhani.raoudha6@gmail.com>
2023-08-20 03:11:05 -0700

    func

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,19 +6,20 @@
  */
 void print_square(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
 
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-		}
-	}
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+}
+}
 }
