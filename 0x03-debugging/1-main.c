5567044a4400eef0560494f313bb94dca1721a59
raoudha123456789 <romdhani.raoudha6@gmail.com>
2023-08-20 02:29:38 -0700

    debug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,13 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
-
 i = 0;
 
 /*
 * Commented out the infinite loop
 * while (i < 10)
 * {
-*         putchar(i);
+* putchar(i);
 * }
 */
 
