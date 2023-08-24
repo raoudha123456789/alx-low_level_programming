4b9c8428c1033c3db6053f1a545dfa7800f0ee65
raoudha123456789 <romdhani.raoudha6@gmail.com>
2023-08-24 14:47:51 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,42 +1,46 @@
 #include "main.h"
+
 /**
- * infinite_add - Adds two numbers as strings
- * @n1: The first number as a string
- * @n2: The second number as a string
- * @r: The buffer to store the result
- * @size_r: The size of the buffer
- * Return: A pointer to the result string or 0 if the result cannot fit in r
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int carry = 0;
-int sum;
-int i = 0;
+int i, j, k, l, m, n;
 
-while (*n1 != '\0' || *n2 != '\0' || carry != 0)
-{
-int digit1 = (*n1 != '\0') ? *n1 - '0' : 0;
-int digit2 = (*n2 != '\0') ? *n2 - '0' : 0;
-sum = digit1 + digit2 + carry;
-if (i < size_r - 1)
-{
-r[i] = '0' + (sum % 10);
-i++;
-}
-else
-{
+for (i = 0; n1[i]; i++)
+;
+for (j = 0; n2[j]; j++)
+;
+if (i > size_r || j > size_r)
 return (0);
-}
-carry = sum / 10;
-if (*n1 != '\0')
-n1++;
-if (*n2 != '\0')
-n2++;
-}
-if (i < size_r)
+m = 0;
+for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 {
-r[i] = '\0';
+n = m;
+if (i >= 0)
+n += n1[i] - '0';
+if (j >= 0)
+n += n2[j] - '0';
+if (i < 0 && j < 0 && n == 0)
+{
+break;
+}
+m = n / 10;
+r[k] = n % 10 + '0';
+}
+r[k] = '\0';
+if (i >= 0 || j >= 0 || m)
+return (0);
+for (k -= 1, l = 0; l < k; k--, l++)
+{
+m = r[k];
+r[k] = r[l];
+r[l] = m;
+}
 return (r);
-}
-return (0);
 }
